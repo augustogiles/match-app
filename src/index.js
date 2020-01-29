3e6bc3ea349bed726dbea43336b389b328d3a1da
Augusto Costa <augustogiles@gmail.com>
2020-01-29 22:47:50 +0000

    removing commented code

--- src/index.js
+++ src/index.js
@@ -20,55 +20,6 @@ const TableObject = objects => {
   );
 };
 
-// class Team extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: null,
-//       stats: {},
-//     };
-//   }
-
-//   componentDidMount(){
-//     fetch("/teams/" + this.props.match.params.index).then(data => 
-//     {
-//         this.setState({
-//           data: data,
-//           stats: computeTeamStats(data.id, data.results)
-//         })
-//     });
-
-//   }
-
-//   componentWillReceiveProps(nextProps) {
-//     const oldIdData = this.state.data.id;
-//     const newIdData = parseInt(nextProps.match.params.index, 10);
-//     if (oldIdData !== newIdData) {
-//       fetch("/teams/" + newIdData).then(data => 
-//         {
-//           this.setState({
-//             data: data,
-//             stats: computeTeamStats(data.id, data.results)
-//           })
-//         });
-//     }
-//   }
-
-//   render() {
-//     const { data } = this.state;
-//     if (!data) return <div>loading...</div>;
-
-//     return (
-//       <div className="team">
-//         <h1>Team {data.name}</h1>
-
-//         <h2>Games</h2>
-//         <Results results={data.results} />
-//       </div>
-//     );
-//   }
-// }
-
 class Weeks extends React.Component {
   constructor(props) {
     super(props);
