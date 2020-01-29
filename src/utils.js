97e9f54ae4af92274e8c5df262b8c3e0a53b908c
Augusto Costa <augustogiles@gmail.com>
2020-01-29 22:46:14 +0000

    refactor: create utils file

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+// TASK #3 - compute team stats
+export function computeTeamStats(id, results) {
+  return {};
+}
+
+
+// TASK #4 - create a table of results
+function computeTable(teams, weeksMatches) {
+  return [];
+}
