b365b59899ca01893e2129a2c4cc4ab92ebb9f44
Augusto Costa <augustogiles@gmail.com>
2020-01-30 08:43:05 +0000

    refactor(Header): isolating Header component

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Header = () => (
+  <div className="header">
+    <ul className="unstyled">
+      <li>
+        <Link to="/table">table</Link>
+      </li>
+      <li>
+        <Link to="/weeks/1">weeks</Link>
+      </li>
+    </ul>
+  </div>
+);
