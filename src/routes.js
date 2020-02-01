4f1de40b9c72daae079072bf00339b70ed4fc975
Augusto Costa <augustogiles@gmail.com>
2020-02-01 11:45:51 +0000

    Routes: adjusts in routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Table from './Table/Table';
 import Weeks  from './Weeks/Weeks';
 import Team from './Team/Team';
 
 export const Routes = () => (
-  // <HashRouter>
-      <Switch>
-        <Route path="/weeks/:index" component={Weeks} />
-        <Route path="/teams/:index" component={Team} />
-        <Route path="/table" component={Table} />
-        <Redirect from="/" to="/weeks/1" />
-      </Switch>
-  //</HashRouter>
+  <Switch>
+    <Redirect exact from="/" to="/weeks/1" />
+    <Route exact path="/weeks/:index" component={Weeks} />
+    <Route exact path="/teams/:index" component={Team} />
+    <Route exact path="/table" component={Table} />
+  </Switch>
 );
