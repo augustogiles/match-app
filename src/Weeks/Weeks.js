cdc89f14d2873281625f84db7020d4891f2a7195
Augusto Costa <augustogiles@gmail.com>
2020-02-02 22:47:18 +0000

    style: styling week component ( unfinished )

--- src/Weeks/Weeks.js
+++ src/Weeks/Weeks.js
@@ -3,6 +3,61 @@ import { Link } from 'react-router-dom';
 import { Results } from '../Results/Results' 
 import { fetch } from '../services/api'
 
+import styled from 'styled-components'
+
+const WeekStyled = styled.div `
+  width: 800px;
+  height: 1000px;
+  max-height: calc(100% - 200px);
+
+  background-color: white;
+  margin: 120px auto 50px auto;
+  
+  border-radius: 8px;
+  border-collapse: collapse;
+  box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.20);
+
+  overflow: auto;
+
+  h2 {
+    text-align: center;
+    font-weight: 200;
+    color: rgba(0,0,0,.54);
+  }
+`;
+
+const WeekHeaderStyled = styled.div `
+  background-color: #999999;
+  width: 100%;
+
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  
+  display: flex;
+  align-items: center;
+
+  h1 {
+    float: left;
+    padding: 12px 16px;
+    font-weight: 400;
+
+    color: #ffffff;
+  }
+`;
+
+const WeekListStyled = styled.ul `
+
+`;
+
+const WeekListItem = styled.li `
+  float: left;
+  padding: 12px 16px;
+
+  color: ${({selected}) => selected ? '#ffffff;' : '#dddddd;'};
+  border-bottom: ${({selected}) => selected ? '2px solid #ffffff;' : 'none;'};
+  background-color: ${({selected}) => selected ? 'rgba(0,0,0,.1);' : 'inherit;'};
+`;
+
 export default class Weeks extends Component {
   constructor(props) {
     super(props);
@@ -37,20 +92,20 @@ export default class Weeks extends Component {
     if (!data.length) return <div>loading...</div>;
 
     return (
-      <div className="weeks">
-        <h1>Weeks</h1>
-        <div className="week-chooser">
-          <ul className="unstyled">
+      <WeekStyled>
+        <WeekHeaderStyled>
+          <h1>Weeks</h1>
+          <WeekListStyled>
             {this.state.data.map((week, weekNumber) => (
-              <li key={weekNumber}>
+              <WeekListItem key={weekNumber}>
                 <Link to={`/weeks/${weekNumber}`}>{weekNumber}</Link>
-              </li>
+              </WeekListItem>
             ))}
-          </ul>
-        </div>
+          </WeekListStyled>
+        </WeekHeaderStyled>
         <h2>Results for week #{this.state.chosenWeek}</h2>
         <Results results={this.state.data[this.state.chosenWeek]} />
-      </div>
+      </WeekStyled>
     );
   }
 }
