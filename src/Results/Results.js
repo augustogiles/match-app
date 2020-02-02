e168a58143703ae9bf73ef672d3aac9b8f019c1e
Augusto Costa <augustogiles@gmail.com>
2020-02-02 22:32:45 +0000

    style: styling result component

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styled from 'styled-components'
+
+const ResultsContainerStyled = styled.div `
+  margin: 20px auto;
+  width: 80%;
+`;
+
+const ResultStyled = styled.div `
+
+  text-align: center;
+  padding: 20px 16px;
+  margin: 20px 0px;
+  color: rgba(0,0,0,.87);
+
+  border-radius: 8px;
+  box-shadow: 0px 2px 6px 0px rgba(0,0,0,0.20);
+
+  transition-property: box-shadow margin-top filter;
+  -o-transition-duration: .1s;
+
+  &:hover {
+    background-color: #dddddd;
+    margin-top: 0px;
+    margin-bottom: 5px;
+  }
+`;
+
+const TeamResultStyled = styled(Link)`
+  position: relative;
+  &:hover {
+    color: grey;
+  }
+`;
 
 const Result = props => {
   const { teams, teamIds, score } = props;
@@ -18,16 +51,20 @@ const Result = props => {
     [homeTeam, visitorTeam] = boldWinner();    
   }
 
-  return <p><Link to={`/teams/${teamIds[0]}`}>{homeTeam}</Link> {score[0]} vs {score[1]} <Link to={`/teams/${teamIds[1]}`}>{visitorTeam}</Link> </p>
+  return (
+    <ResultStyled className="result">
+        <TeamResultStyled to={`/teams/${teamIds[0]}`}>{homeTeam}</TeamResultStyled> {score[0]} vs {score[1]} <TeamResultStyled to={`/teams/${teamIds[1]}`}>{visitorTeam}</TeamResultStyled> 
+    </ResultStyled>
+  );
 
 };
 
 export const Results = ({ results }) => {
     return (
-      <div className="results">
+      <ResultsContainerStyled className="container">
         {results.map((result) => {
           return <Result key={`${result.teams[0]}_${result.teams[1]}`} {...result} />;
         })}
-      </div>
+      </ResultsContainerStyled>
     );
 }
