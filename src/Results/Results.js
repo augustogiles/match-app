fd8a79f67682b8743b706169dc42be81b5a462c8
Augusto Costa <augustogiles@gmail.com>
2020-02-03 21:48:27 +0000

    refactor(Result): creating and importing Result styled file; sombe bug-fix in week index selected

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,65 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import styled from 'styled-components'
-
-const ResultsContainerStyled = styled.div `
-  margin: 20px auto;
-  padding: 40px 20px;
-  
-  width: 80%;
-
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-
-  overflow: auto;
-
-`;
-
-const ResultStyled = styled.div `
-
-  padding: 20px 16px;
-  color: rgba(0,0,0,.87);
-
-  border-radius: 8px;
-  box-shadow: 0px 2px 6px 0px rgba(0,0,0,0.20);
-
-  transition-property: box-shadow margin-top filter;
-  transition-duration: .2s;
-
-  &:hover {
-    background-color: #dddddd;
-    margin-top: 5px;
-    margin-bottom: 0px;
-    box-shadow: 0px 2px 10px 0px rgba(0,0,0,0.20);
-  }
-`;
-
-const ImgResultStyled = styled.img `
-  width: 24px;
-  height: 24px;
-  margin: 12px;
-`;
-
-const TeamResultStyled = styled(Link)`
-  position: relative;
-  bottom: 20px;
-
-  transition-property: color filter;
-  transition-duration: .2s;
-  &:hover {
-    color: grey;
-  }
-`;
-
-const TeamScoreStyled = styled.div `
-  
-  float: right;
-  top: 12px;
-  position: relative;
-  
-`;
+import { ResultsContainerStyled, ResultStyled, ImgResultStyled, TeamResultStyled, TeamScoreStyled} from './Results.styled'
 
 const Result = props => {
   const { teams, teamIds, score } = props;
---
+++ src/Results/Results.styled.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+import { Link } from 'react-router-dom';
+
+export const ResultsContainerStyled = styled.div `
+  margin: 20px auto;
+  padding: 40px 20px;
+  
+  width: 80%;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+
+  overflow: auto;
+
+`;
+
+export const ResultStyled = styled.div `
+
+  padding: 20px 16px;
+  color: rgba(0,0,0,.87);
+
+  border-radius: 8px;
+  box-shadow: 0px 2px 6px 0px rgba(0,0,0,0.20);
+
+  transition-property: box-shadow margin-top filter;
+  transition-duration: .2s;
+
+  &:hover {
+    background-color: #dddddd;
+    margin-top: 5px;
+    margin-bottom: 0px;
+    box-shadow: 0px 2px 10px 0px rgba(0,0,0,0.20);
+  }
+`;
+
+export const ImgResultStyled = styled.img `
+  width: 24px;
+  height: 24px;
+  margin: 12px;
+`;
+
+export const TeamResultStyled = styled(Link)`
+  position: relative;
+  bottom: 20px;
+
+  transition-property: color filter;
+  transition-duration: .2s;
+  &:hover {
+    color: grey;
+  }
+`;
+
+export const TeamScoreStyled = styled.div `
+  
+  float: right;
+  top: 12px;
+  position: relative;
+  
+`;
